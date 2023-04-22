56a99b5151a3f9d06075734c68aadf09458f917d
gluong <16323642+guy-luong@users.noreply.github.com>
2023-04-22 10:21:57 +0200

    Clean: Card subclasses -> Functions

--- sketch.js
+++ sketch.js
@@ -55,78 +55,70 @@ class Card {
   }
 }
 
-class BaseAttackCard extends Card {
-  /**
-   * 
-   * @param {Number} damage 
-   */
-  constructor(damage) {
-    super("Base attack", "A simple attack.", (target) => target.takeHit(damage));
-  }
+/**
+ * 
+ * @param {Number} damage 
+ * @returns 
+ */
+const BaseAttackCard = (damage) => {
+  return new Card("Base attack", "A simple attack.", (target) => target.takeHit(damage));
 }
 
-class BernoulliCard extends Card {
-  /**
-   * 
-   * @param {Number} parameter The probability of success for each hit
-   * @param {Number} damage The damage dealt on each hit
-   */
-  constructor(parameter, damage) {
-    super("Heads or tails", "A strong attack. Accuracy: " + (parameter * 100) + "%", (target) => {
-      if (Math.random() > parameter) {
-        console.log("Lucky! A strong hit landed!");
-        target.takeHit(damage);
-      } else {
-        let rand = int(Math.random() * 3);
-        switch (rand) {
-          case 0: console.log("Shucks! You slipped on a banana! The attack missed."); break;
-          case 1: console.log("You had an urge to sneeze! A-A-Achoo! The attack missed."); break;
-          case 2: console.log("The enemy swiftly dodged the attack. The attack missed."); break;
-        }
+/**
+ * 
+ * @param {Number} parameter The probability of success for each hit
+ * @param {Number} damage The damage dealt on each hit
+ */
+const BernouilliCard = (parameter, damage) => {
+  return new Card("Heads or tails", "A strong attack. Accuracy: " + (parameter * 100) + "%", (target) => {
+    if (Math.random() > parameter) {
+      console.log("Lucky! A strong hit landed!");
+      target.takeHit(damage);
+    } else {
+      let rand = int(Math.random() * 3);
+      switch (rand) {
+        case 0: console.log("Shucks! You slipped on a banana! The attack missed."); break;
+        case 1: console.log("You had an urge to sneeze! A-A-Achoo! The attack missed."); break;
+        case 2: console.log("The enemy swiftly dodged the attack. The attack missed."); break;
       }
-    });
-  }
+    }
+  });
+};
+
+/**
+ * 
+ * @param {Number} damage The damage dealt on each hit
+ */
+const CoinThrowCard = (damage) => {
+  return BernouilliCard(0.5, damage);
 }
 
-class CoinThrowCard extends BernoulliCard {
-  /**
-   * 
-   * @param {String} name
-   * @param {Number} damage The damage dealt on each hit
-   */
-  constructor(name, damage) {
-    super(name, 0.5, damage);
-  }
-}
+/**
+ * 
+ * @param {Number} parameter The probability of success for each hit
+ * @param {Number} damage The damage dealt on each hit
+ */
+const GeometricCard = (parameter, damage) => {
+  return new Card("Geometric card", "A flurry of attacks, ending when an attack misses. Accuracy: " + (parameter * 100) + "%", (target) => {
+    let i = 0;
+    while (Math.random() > (1 - parameter)) {
+      target.takeHit(damage);
+      i++;
+    }
+    if (target.isAlive()) {
+      switch (i) {
+        case 0: console.log(target.name + " was ready for it and dodged the attack."); break;
+        case 1:
+          console.log(target.name + " SDI'd so hard that he is already out of range."); break;
+        default:
+          console.log(target.name + " dodged the last one.");
+      }
+    }
 
-class GeometryCard extends Card {
-  /**
-   * 
-   * @param {Number} parameter The probability of success for each hit
-   * @param {Number} damage The damage dealt on each hit
-   */
-  constructor(parameter, damage) {
-    super("Geometric card", "A flurry of attacks, ending when an attack misses. Accuracy: " + (parameter * 100) + "%", (target) => {
-      let i = 0;
-      while (Math.random() > (1 - parameter)) {
-        target.takeHit(damage);
-        i++;
-      }
-      if (target.isAlive()) {
-        switch (i) {
-          case 0: console.log(target.name + " was ready for it and dodged the attack."); break;
-          case 1:
-            console.log(target.name + " SDI'd so hard that he is already out of range."); break;
-          default:
-            console.log(target.name + " dodged the last one.");
-        }
-      }
-
-      if (i > 1) {
-        console.log(target.name + " took a total of " + damage * i + " damage.");
-      }
-    });
-  }
+    if (i > 1) {
+      console.log(target.name + " took a total of " + damage * i + " damage.");
+    }
+  });
 }
 
 class Deck {
@@ -148,11 +140,11 @@ class Game {
   }
 
   setup() {
-    this.player1.addCard(new BaseAttackCard(10));
-    this.player1.addCard(new CoinThrowCard(20));
-    this.player1.addCard(new GeometryCard(0.8, 4));
+    this.player1.addCard(BaseAttackCard(10));
+    this.player1.addCard(CoinThrowCard(20));
+    this.player1.addCard(GeometricCard(0.8, 4));
 
-    this.player2.addCard(new BaseAttackCard(10));
+    this.player2.addCard(BaseAttackCard(10));
   }
 
   draw() {
